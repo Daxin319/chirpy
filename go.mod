fcae6a6fff3c7bfcd05d566a31c6cec7e9b1fa62
Daxin319 <lylehigg@gmail.com>
2025-02-07 23:30:21 -0700

    finished part 2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module chirpy
+module main
 
-go 1.23.5
+go 1.23.6
--- main.go
+++ main.go
@@ -1,21 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"strconv"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	filepathRoot := http.Dir(".")
+	strippedFileserver := http.StripPrefix("/app", http.FileServer(filepathRoot))
+	port := "8080"
+
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(strippedFileserver))
+	serveMux.HandleFunc("GET /api/healthz", readinessEndpoint)
+	serveMux.HandleFunc("GET /api/metrics", metricsReturn(apiCfg))
+	serveMux.HandleFunc("POST /api/reset", metricsReset(apiCfg))
 
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: serveMux,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func metricsReturn(cfg *apiConfig) func(http.ResponseWriter, *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		hits := strconv.Itoa(int(cfg.fileserverHits.Load()))
+		body := "Hits: " + hits
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(body))
+	})
+}
+
+func metricsReset(cfg *apiConfig) func(http.ResponseWriter, *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		cfg.fileserverHits.Swap(0)
+	})
 }
